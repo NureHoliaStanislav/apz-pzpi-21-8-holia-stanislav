9a4dadf47cd84824989c91f7f8b350169595ab6e
“NureHoliaStanislav” <stanislav.holia@nure.ua>
2024-06-04 06:27:17 +0300

    Added web client

--- Task2/apz-pzpi-21-8-holia-stanislav-task2/fieldforge/fieldforge/settings.py
+++ Task2/apz-pzpi-21-8-holia-stanislav-task2/fieldforge/fieldforge/settings.py
@@ -47,9 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'djangoapp',
     'rest_framework',
+    'corsheaders',
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,3 +145,4 @@ AUTH_USER_MODEL = "djangoapp.User"
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
